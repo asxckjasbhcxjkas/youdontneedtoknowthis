f7c68d024a9ff7d196afb9753e041228d7b976d5
asxckjasbhcxjkas <125092222+asxckjasbhcxjkas@users.noreply.github.com>
2023-02-15 06:48:47 +0530

    Update urls.py

--- ConfessionsProject/urls.py
+++ ConfessionsProject/urls.py
@@ -21,3 +21,5 @@ urlpatterns = [
     path('accounts/', include('Users.urls')),
     path('', include('ConfessionsApp.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
