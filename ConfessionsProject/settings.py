8ab66d56e8728cd1ed71ec72a8c05bd39841febd
asxckjasbhcxjkas <125092222+asxckjasbhcxjkas@users.noreply.github.com>
2023-02-15 20:47:24 +0530

    Update settings.py

--- ConfessionsProject/settings.py
+++ ConfessionsProject/settings.py
@@ -31,7 +31,7 @@ with open('secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  True
 
-ALLOWED_HOSTS = ['.*.sh','.vercel.app']
+ALLOWED_HOSTS = ['.now.sh','.vercel.app']
 
 
 # Application definition
