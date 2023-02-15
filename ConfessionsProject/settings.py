0425093ff7534e332b49817b406f929a6cc20dc4
asxckjasbhcxjkas <125092222+asxckjasbhcxjkas@users.noreply.github.com>
2023-02-15 06:07:33 +0530

    Update settings.py

--- ConfessionsProject/settings.py
+++ ConfessionsProject/settings.py
@@ -87,8 +87,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ConfessionsProject.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
