391761b40db1fb963118aa1eb4fd9ee616da04d3
asxckjasbhcxjkas <125092222+asxckjasbhcxjkas@users.noreply.github.com>
2023-02-17 21:15:22 +0530

    Update settings.py

--- ConfessionsProject/settings.py
+++ ConfessionsProject/settings.py
@@ -89,9 +89,14 @@ WSGI_APPLICATION = 'ConfessionsProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
+
+
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 
 if len(DATABASES['default']) == 0:
 DATABASES = {
