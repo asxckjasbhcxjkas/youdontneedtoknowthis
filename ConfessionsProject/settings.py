3199e32f487ddd48f13abe1bfe1af0760b9d23d7
asxckjasbhcxjkas <125092222+asxckjasbhcxjkas@users.noreply.github.com>
2023-02-17 21:12:58 +0530

    Update settings.py

--- ConfessionsProject/settings.py
+++ ConfessionsProject/settings.py
@@ -95,10 +95,10 @@ DATABASES['default'].update(db_from_env)
 
 if len(DATABASES['default']) == 0:
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
