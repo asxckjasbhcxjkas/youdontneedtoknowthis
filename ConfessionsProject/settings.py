ce6548002398b1125c2c6093c38d74ce759e181b
asxckjasbhcxjkas <125092222+asxckjasbhcxjkas@users.noreply.github.com>
2023-02-17 21:10:56 +0530

    Update settings.py

--- ConfessionsProject/settings.py
+++ ConfessionsProject/settings.py
@@ -89,16 +89,18 @@ WSGI_APPLICATION = 'ConfessionsProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
+if len(DATABASES['default']) == 0:
 DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
