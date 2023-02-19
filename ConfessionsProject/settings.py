23b3cda71a2c7319e8713465911ce0d3e595fde6
asxckjasbhcxjkas <125092222+asxckjasbhcxjkas@users.noreply.github.com>
2023-02-19 08:43:00 +0530

    Update settings.py

--- ConfessionsProject/settings.py
+++ ConfessionsProject/settings.py
@@ -106,14 +106,14 @@ import dj_database_url
 #     }
 # }
 
-
+DATABASES['default'] = dj_database_url.config()
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.ConfessionsProject.sqlite3",
+        "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-DATABASES['default'] = dj_database_url.config()
+
 
 
 # Password validation
